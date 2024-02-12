12a3c8de156ecb3144949123af263687f3fbcd1f
Eli <elijah2307.williams@gmail.com>
2024-02-12 22:03:00 +0000

    addedQrcodeReader

--- ecomap/ecomap/urls.py
+++ ecomap/ecomap/urls.py
@@ -6,6 +6,7 @@ app_name = "ecomap"
 urlpatterns = [
     path("homepage/", views.homepage, name="homepage"),
     path("leaderboard/", views.leaderboard, name="leaderboard"),
-    path("login/", views.login, name="login"),
+    path("login/", views.login, name="hangman"),
     path("hangman/", views.hangman, name="hangman"),
+    path("qrcode/", views.qrcode, name="qrcode"),
 ]
