31b16ebfccb2c1795ad37b83e60ff8ddbd3082ac
Eli <elijah2307.williams@gmail.com>
2024-02-18 18:38:15 +0000

    added register user functionality to register.html and in view.py

--- ecomap/ecomap/models.py
+++ ecomap/ecomap/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class User(models.Model):
-    username = models.CharField(max_length=100)
+    username = models.CharField(max_length=100, unique=True)
     first_name = models.CharField(max_length=100)
     last_name= models.CharField(max_length=100)
     password = models.CharField(max_length=100)
--- ecomap/ecomap/views.py
+++ ecomap/ecomap/views.py
@@ -1,6 +1,8 @@
 from django.http import Http404
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import get_object_or_404, render
-
+from .registerForm import RegisterForm
+from django.shortcuts import redirect
 
 def homepage(request):
     return render(request, "ecomap/homepage.html")
@@ -18,4 +20,20 @@ def qrcode(request):
     return render(request, "ecomap/qrcodereader.html")
 
 def register(request):
-    return render(request, "ecomap/register.html")
+    if request.method == "GET": 
+        return render(request, "ecomap/register.html")
+    
+@csrf_exempt
+def registerUser(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("/register/?success=true")
+        else:
+            form = RegisterForm()
+
+        return redirect("/register/?success=false")
+    
+
+
