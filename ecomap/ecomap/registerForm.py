5f9c55c1476298329f66347b8358f4ef9adea8a7
Eli <elijah2307.williams@gmail.com>
2024-02-20 03:17:12 +0000

    comments on register.html

--- ecomap/ecomap/registerForm.py
+++ ecomap/ecomap/registerForm.py
@@ -1,21 +1,12 @@
 from django import forms
-#from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import NON_FIELD_ERRORS
 from django.forms import ModelForm
 from ecomap.models import User
 
-#from django.contrib.auth.models import User
 
-#class RegisterForm(UserCreationForm):
 class RegisterForm(ModelForm):
-    #firstname=forms.CharField(label="firstname",max_length=100, required=True)
-    #lastname=forms.CharField(label="lastname",max_length=100, required=True)
-    #username=forms.CharField(label="username",max_length=100, required=True)
-    #password=forms.CharField(label="password",max_length=512, required=True)
     userType=forms.CharField(label="userType",max_length=20, required=False)
     
-    
-
     class Meta:
         model = User
         fields = ["username","first_name","last_name","password","userType"]
