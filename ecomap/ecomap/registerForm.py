2905366fd63a3f2a3a4c5e7252c84f3aab1d9b8d
Chit Hin Ernest Cheong <103903420+ernest216@users.noreply.github.com>
2024-02-25 19:47:02 +0000

    update on migration

--- ecomap/ecomap/registerForm.py
+++ ecomap/ecomap/registerForm.py
@@ -6,10 +6,12 @@ from ecomap.models import User
 
 class RegisterForm(ModelForm):
     userType=forms.CharField(label="userType",max_length=20, required=False)
+    score=forms.IntegerField(label="score",required=False)
+    streak=forms.IntegerField(label="streak",required=False)
     
     class Meta:
         model = User
-        fields = ["username","first_name","last_name","password","userType"]
+        fields = ["username","first_name","last_name","password","userType","score","streak"]
         error_messages = {
             NON_FIELD_ERRORS: {
                 "unique_together": "%(model_name)s's %(field_labels)s are not unique.",
