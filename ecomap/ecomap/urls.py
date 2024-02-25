0ca5cd0d8c421fbd059e0598d6eb5e4b03bc1383
Eli <elijah2307.williams@gmail.com>
2024-02-25 21:46:53 +0000

    made localhost.com/ point to homepage instead of /homepage

--- ecomap/ecomap/urls.py
+++ ecomap/ecomap/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = "ecomap"
 urlpatterns = [
-    path("homepage/", views.homepage, name="homepage"),
+    path("", views.homepage, name="homepage"),
     path("leaderboard/", views.leaderboard, name="leaderboard"),
     path("login/", views.login, name="hangman"),
     path("hangman/", views.hangman, name="hangman"),
