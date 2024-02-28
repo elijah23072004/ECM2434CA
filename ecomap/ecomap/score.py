0f30547260a77b0c40205857401b781e54399357
Eli <elijah2307.williams@gmail.com>
2024-02-28 22:15:15 +0000

    made score.py add to score instead of only having high score

--- ecomap/ecomap/score.py
+++ ecomap/ecomap/score.py
@@ -1,8 +1,6 @@
 from django.contrib.auth.models import User
 from .models import User as EcomapUser
-
-#function that returns -1 if invalid user, 0 if user's high score is higher than score passed and 1 if user's score is lower
-#and saves score to user if it is higher then current score
+#function that is passed score and adds it to the user passed score
 def handleScore(user, score):
     currentUser=EcomapUser.objects.get(username=user.username)
     #checks if a user is in the request if they are not it is erronous so return -1 
@@ -11,13 +9,9 @@ def handleScore(user, score):
     score=int(score)
     userTopScore=currentUser.score
 
-    #if userhighscore is higher then scored passed then dont save new score 
-    if(userTopScore>score):
-        return 0;
-    
-    #if highscore is lower then save new score as high score
-    currentUser.score=userTopScore
-    EcomapUser.objects.filter(username=user.username).update(score=score)
+    newScore= score+userTopScore 
+    currentUser.score=newScore
+    EcomapUser.objects.filter(username=user.username).update(score=newScore)
 
 
     return 1;
