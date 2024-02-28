7f6d1380eae9a008ee9ee24e471a6cf23218aff1
Chit Hin Ernest Cheong <103903420+ernest216@users.noreply.github.com>
2024-02-28 18:46:25 +0000

    add code comments

--- ecomap/ecomap/urls.py
+++ ecomap/ecomap/urls.py
@@ -3,16 +3,18 @@ from django.urls import path
 from . import views
 
 app_name = "ecomap"
+
+# Define the URL patterns for the ecomap app
 urlpatterns = [
-    path("", views.homepage, name="homepage"),
-    path("leaderboard/", views.leaderboard, name="leaderboard"),
-    path("login/", views.login, name="login"),
-    path("hangman/", views.hangman, name="hangman"),
-    path("wordle/", views.wordle, name="wordle"),
-    path("qrcode/", views.qrcode, name="qrcode"),
-    path("register/",views.register, name="register"),
-    path("registerUser/",views.registerUser, name="createUser"),
-    path("loginUser/",views.loginUser, name="loginUser"),
-    path("logout/",views.userlogout,name="logout"),
-    path("wheel/", views.gameWheel, name="wheel"),
+    path("", views.homepage, name="homepage"),  # Homepage URL
+    path("leaderboard/", views.leaderboard, name="leaderboard"),  # Leaderboard URL
+    path("login/", views.login, name="login"),  # Login URL
+    path("hangman/", views.hangman, name="hangman"),  # Hangman URL
+    path("wordle/", views.wordle, name="wordle"),  # Wordle URL
+    path("qrcode/", views.qrcode, name="qrcode"),  # QR Code URL
+    path("register/", views.register, name="register"),  # Register URL
+    path("registerUser/", views.registerUser, name="createUser"),  # Create User URL
+    path("loginUser/", views.loginUser, name="loginUser"),  # Login User URL
+    path("logout/", views.userlogout, name="logout"),  # Logout URL
+    path("wheel/", views.gameWheel, name="wheel"),  # Game Wheel URL
 ]
