b198ac8d62af8bfbbe5d1372d90cdb6aac16ded2
Eli <elijah2307.williams@gmail.com>
2024-02-28 18:54:03 +0000

    comments on urls.py

--- ecomap/ecomap/urls.py
+++ ecomap/ecomap/urls.py
@@ -4,17 +4,17 @@ from . import views
 
 app_name = "ecomap"
 urlpatterns = [
-    path("", views.homepage, name="homepage"),
-    path("leaderboard/", views.leaderboard, name="leaderboard"),
-    path("login/", views.login, name="login"),
-    path("hangman/", views.hangman, name="hangman"),
-    path("wordle/", views.wordle, name="wordle"),
-    path("qrcode/", views.qrcode, name="qrcode"),
-    path("register/",views.register, name="register"),
-    path("registerUser/",views.registerUser, name="createUser"),
-    path("loginUser/",views.loginUser, name="loginUser"),
-    path("logout/",views.userlogout,name="logout"),
-    path("wheel/", views.gameWheel, name="wheel"),
-    path("sendScore/", views.submitScore, name="submitScore"),
-    path("getScores/", views.getUserScores,name="getScores"),
+    path("", views.homepage, name="homepage"),  # Homepage URL
+    path("leaderboard/", views.leaderboard, name="leaderboard"),  # Leaderboard URL
+    path("login/", views.login, name="login"),  # Login URL
+    path("hangman/", views.hangman, name="hangman"),  # Hangman URL
+    path("wordle/", views.wordle, name="wordle"),  # Wordle URL
+    path("qrcode/", views.qrcode, name="qrcode"),  # QR Code URL
+    path("register/", views.register, name="register"),  # Register URL
+    path("registerUser/", views.registerUser, name="createUser"),  # Create User URL
+    path("loginUser/", views.loginUser, name="loginUser"),  # Login User URL
+    path("logout/", views.userlogout, name="logout"),  # Logout URL
+    path("wheel/", views.gameWheel, name="wheel"),  # Game Wheel URL
+    path("sendScore/", views.submitScore, name="submitScore"), #Submit Score URL
+    path("getScores/", views.getUserScores,name="getScores"), #Get Score URL
 ]
