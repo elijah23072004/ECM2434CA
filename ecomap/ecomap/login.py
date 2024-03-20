a10277d90caca9f58f18977cbcf91f891818e1ac
Tharun Veluri <96026915+TharunVeluri03@users.noreply.github.com>
2024-03-20 02:01:09 +0000

    Update login.py
    Updated django to check invalid fields and provide invalid message.

--- ecomap/ecomap/login.py
+++ ecomap/ecomap/login.py
@@ -1,15 +1,36 @@
+#from .models import User
+
+
+#def loginAuth(username, password):
+#    try:
+#        user = User.objects.get(username=username)
+#    except Exception as e:
+#        return False, e
+
+#    if password == user.password:
+#        return True, "Valid Login"
+#    else:
+#        return False, "Invalid Password"
+
+from django.shortcuts import render
 from .models import User
+from .utils import loginAuth
 
-
-def loginAuth(username, password):
-    try:
-        user = User.objects.get(username=username)
-    except Exception as e:
-        return False, e
-
-    if password == user.password:
-        return True, "Valid Login"
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('InputtedUsername')
+        password = request.POST.get('InputtedPassword')
+        authenticated, message = loginAuth(username, password)
+        if authenticated:
+            # If login is successful, redirect to the home page or any other desired page
+            return redirect('home')
+        else:
+            # If login fails, reload the login page with an error message
+            error_message = message
+            return render(request, 'login.html', {'error_message': error_message})
     else:
-        return False, "Invalid Password"
+        # If it's a GET request, render the login page
+        return render(request, 'login.html')
+
 
 
